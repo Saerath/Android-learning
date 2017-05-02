4d5a976f2869e91974a46e9985efb9923521ca3b
Saerath <Saerath13@gmail.com>
2017-05-02 11:31:26 +0300

    Some changes

--- src/BirchBranch.java
+++ src/BirchBranch.java
@@ -4,10 +4,5 @@ public class BirchBranch implements Branch {
 	public void grow(){
 		System.out.println("New branch grew");
 	}
-	
-	public static void main(String [ ] args){
-		Branch branch = new BirchBranch();
-		branch.grow();
-	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String [ ] args){
+		Branch branch = new BirchBranch();
+		branch.grow();
 	}
 
 }
