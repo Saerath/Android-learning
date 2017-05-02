34f71f1cdb0a45329f61380dca7241bb0aa90a7f
Saerath <saerath13@gmail.com>
2017-05-02 10:22:31 +0300

    First commit

---
+++ src/BirchBranch.java
@@ -0,0 +1,13 @@
+
+interface BirchBranch{
+	void grow();
+}
+
+public class BirchBranch implements BirchBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
