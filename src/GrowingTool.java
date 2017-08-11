35c422cf4af21bf090cd11c0e20361278debade3
Saerath <Saerath13@gmail.com>
2017-08-11 13:04:34 +0300

    Try/catch. Parser

--- src/GrowingTool.java
+++ src/GrowingTool.java
@@ -12,34 +12,53 @@ public class GrowingTool {
 		
 	}
 	
-	public void createBranch() throws IOException{
+	public void createBranch(){
 		System.out.println("Choose oak/birch branch");
-		String branchType = reader.readLine();
-		
-		System.out.println("Name for new branch:");
-		String attachingBranch = reader.readLine().toUpperCase();
-		
-		branchFactory.create(branchType, attachingBranch);
-	}
-	
-	public void selectBranchToAttach(Branch branch) throws IOException{
-		System.out.println("Select branch to attach");
-		String attachingBranch = reader.readLine();
-		if(branch.getBranchFromMap(attachingBranch).isAttached()){
-			System.out.println("Selected branch attached already");
-		}else{
-			System.out.println("Select place to attach");
-			String attachebleBranch = reader.readLine();
-			branch.getBranchFromMap(attachebleBranch).attach(branch.getBranchFromMap(attachingBranch));
+		String branchType;
+		try {
+			branchType = reader.readLine();
+			System.out.println("Name for new branch:");
+			String attachingBranch = reader.readLine().toUpperCase();
 			
-			branch.showAttached("");
+			branchFactory.create(branchType, attachingBranch);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 	}
 	
-	public void growBranch(Branch branch) throws IOException{
+	public void selectBranchToAttach(Branch branch){
+		System.out.println("Select branch to attach");
+		String attachingBranch;
+		try {
+			attachingBranch = reader.readLine();
+			if(branch.getBranchFromMap(attachingBranch).isAttached()){
+				System.out.println("Selected branch attached already");
+			}else{
+				System.out.println("Select place to attach");
+				String attachebleBranch = reader.readLine();
+				branch.getBranchFromMap(attachebleBranch).attach(branch.getBranchFromMap(attachingBranch));
+				
+				branch.showAttached("");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void growBranch(Branch branch){
 		System.out.println("Select branch");
-		String attachebleBranch = reader.readLine();
-		branch.getBranchFromMap(attachebleBranch).grow();
+		String attachebleBranch;
+		try {
+			attachebleBranch = reader.readLine();
+			branch.getBranchFromMap(attachebleBranch).grow();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		branch.showAttached("");
 	}
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,10 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
-
+	Pattern p = Pattern.compile("aa");
+	Matcher m = p.matcher(null);
+	
+	
+	
 }
